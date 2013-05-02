f46136f4faf985c2be25375277d94c62d635f390
eSim <egithub@laposte.net>
2013-05-02 13:27:41 +0300

    effacage commentaires

--- Modele.rb
+++ Modele.rb
@@ -13,8 +13,6 @@
 # == un compteur de tour de jeu
 #
 
-#faisage de comm pour voir :D
-#2e faisage de comm yo
 require './AffichageDebug.rb'
 require './Enum/EnumStadePartie.rb'
 require './Joueur.rb'
