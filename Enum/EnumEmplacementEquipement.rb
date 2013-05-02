9331ce33842ba499049f5bba101dc212a52a8d20
eSim <egithub@laposte.net>
2013-05-02 16:49:01 +0300

    Rajout d'un type Enum pour l'emplacement d'un équipement

---
+++ Enum/EnumEmplacementEquipement.rb
@@ -0,0 +1,18 @@
+class EnumEmplacementEquipement
+  @@ARMURE                  = 1
+  @@EPEE                    = 2
+  @@BOTTES                  = 3
+
+  def EnumStadePartie.ARMURE
+    return @@ARMURE
+  end
+    
+  def EnumStadePartie.EPEE
+    return @@EPEE
+  end
+  
+  def EnumStadePartie.BOTTES
+    return @@BOTTES
+  end
+  
+end
