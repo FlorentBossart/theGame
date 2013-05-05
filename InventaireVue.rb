1eedf0320ed0313df9b7885dac4c15f0e12b3d91
Jérémy <jeremyvoisin@MacBook-Pro-de-Jeremy-Voisin.local>
2013-05-06 00:18:10 +0200

    Blabla obligatoire de commit

--- InventaireVue.rb
+++ InventaireVue.rb
@@ -1,4 +1,5 @@
 require 'gtk2'
+require 'Bibliotheque/ReferencesGraphiques.rb'
 class InventaireVue
     @refGraphiques
     @vueInventaire
@@ -6,42 +7,52 @@ class InventaireVue
     @inventaire
     @nbItemH
     @nbItemL
+    @imageItemSelected
+    @vbox
+    @hbox
+    @boutonInteraction
     
     def initialize
         @nbItemH = 5
         @nbItemL = 5
         @inventaire = Array.new(@nbItemH){|x| Array.new(@nbItemL){|y| Gtk::Image.new()}}
         @eventInventaire = Array.new(@nbItemH){|x| Array.new(@nbItemL){|y| Gtk::EventBox.new()}}
+        
+        @imageItemSelected = Gtk::Image.new("img/coloris_noir.png")
+        @vbox = Gtk::VBox.new(false,10)
+        @hbox = Gtk::HBox.new(false,10)
         @vueInventaire = Gtk::Table.new(@nbItemH,@nbItemL,true);
+        @boutonInteraction = Gtk::Button.new("Jeter")
         
         0.upto(@nbItemH-1) do |x|
             0.upto(@nbItemL-1)do |y|
-                @inventaire[x][y].file = "coloris_noir.png"
+                @inventaire[x][y].file = "img/coloris_noir.png"
                 @eventInventaire[x][y].add(@inventaire[x][y])
-                
-                #Associer l'événement ici
                 
                 @vueInventaire.attach(@eventInventaire[x][y],y,y+1,x,x+1);
             end
         end
-        
-        window = Gtk::Window.new();
-        window.signal_connect('destroy') {
-            Gtk.main_quit();
-        }
-        
-        window.add(@vueInventaire)
-        window.show_all
-        
+        @vbox.pack_start(@vueInventaire,true,true,0)
+        @vbox.pack_start(@hbox,true,true,0)
+        @hbox.pack_start(@imageItemSelected,true,true,0)
+        @hbox.pack_start(@boutonInteraction,true,true,0)
     end
     
-    def afficherInventaire(joueur)
-        joueur.inventaire.item.each{|x|
-            @inventaire[x%nbItemH][y/nbItemL].file=((@referencesGraphiques.getRefGraphique(x.intitule.downcase)));
-        }
+    def afficherInventaireModeInteractif(joueur,fenetre, mode)
+        
+        fenetre.add(@vbox)
+        #joueur.inventaire.item.each{|x|
+        #   @inventaire[x%nbItemH][y/nbItemL].file=((@referencesGraphiques.getRefGraphique(x.intitule.downcase)));
+        #}
     end
+    
 end
 
 Gtk.init()
-InventaireVue.new
+window = Gtk::Window.new();
+window.signal_connect('destroy') {
+    Gtk.main_quit();
+}
+InventaireVue.new.afficherInventaireModeInteractif(nil,window,"Test")
+window.show_all
 Gtk.main()
