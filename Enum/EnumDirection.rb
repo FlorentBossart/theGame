ce1c18a6b60bc4c07e80126d3fc4571f2717c47f
xvypx <vyannisv@aol.com>
2013-05-06 11:48:28 +0200

    Modification règles de codages

--- Enum/EnumDirection.rb
+++ Enum/EnumDirection.rb
@@ -1,25 +1,66 @@
+#!/usr/bin/env ruby
+
+##
+# Fichier        : EnumDirection.rb
+# Auteur         : L3SPI - Groupe de projet B
+# Fait partie de : TheGame
+#
+# Cette classe représente les différents types de directions possibles définie par:
+#* Une direction NORD
+#* Une direction SUD
+#* Une direction EST
+#* Une direction OUEST
+#
+
 class EnumDirection
-  @@NORD  = 1
-  @@SUD   = 2
-  @@EST   = 3
-  @@OUEST = 4
-  
-  def EnumDirection.NORD
-    return @@NORD
-  end
-  
-  
-  def EnumDirection.SUD
+   
+   @@NORD  = 1
+   @@SUD   = 2
+   @@EST   = 3
+   @@OUEST = 4
+   
+   
+   ##
+   #===Indique la direction NORD.
+   #
+   #===Retourne:
+   #* <b>direction :</b> la direction NORD
+   #
+   def EnumDirection.NORD
+      return @@NORD
+   end
+
+   
+   ##
+   #===Indique la direction SUD.
+   #
+   #===Retourne:
+   #* <b>direction :</b> la direction SUD
+   #
+   def EnumDirection.SUD
       return @@SUD
-  end
-    
-    
-  def EnumDirection.EST
+   end
+
+   
+   ##
+   #===Indique la direction EST.
+   #
+   #===Retourne:
+   #* <b>direction :</b> la direction EST
+   #
+   def EnumDirection.EST
       return @@EST
-  end
-    
-    
-  def EnumDirection.OUEST
+   end
+
+   
+   ##
+   #===Indique la direction OUEST.
+   #
+   #===Retourne:
+   #* <b>direction :</b> la direction OUEST
+   #
+   def EnumDirection.OUEST
       return @@OUEST
-  end
+   end
+   
 end
