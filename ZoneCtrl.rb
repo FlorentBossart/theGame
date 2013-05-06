e530665604b507892990a50f0f0f1de53a377839
dragonxav <xavboxxavto@msn.com>
2013-05-06 10:37:30 +0200

    maj commentaire

--- ZoneCtrl.rb
+++ ZoneCtrl.rb
@@ -1,25 +1,21 @@
-#!/usr/bin/env ruby 
+#!/usr/bin/env ruby
 
-## 
-# Fichier            : ZoneCtrl.rb 
-# Auteur            : L3SPI - Groupe de projet B 
-# Fait partie de : TheGame 
-# 
+##
+# Fichier            : ZoneCtrl.rb
+# Auteur            : L3SPI - Groupe de projet B
+# Fait partie de : TheGame
+#
 # Cette classe représente la zone de controle en bas a droite du jeu, avec les boutons d'intération
-# 
+#
 require 'gtk2'
 require './Bibliotheque/ReferencesGraphiques.rb'
 require './XMLReader/XmlRefGraphiquesReader.rb'
 require './Controller.rb'
 
-
-
-
 class ZoneCtrl <  Gtk::Frame
   @referencesGraphiques
   @vue
   @controller
-  
   def initialize(vue,controller)
     super(); #Frame
     #Gtk.init();
@@ -31,12 +27,11 @@ class ZoneCtrl <  Gtk::Frame
 
     #Gtk.main();
   end
-  
+
   def ZoneCtrl.creer(vue,controller)
     new(vue,controller)
   end
-  
-  
+
   def initInterface()
     #creation image bouton
     gauche = Gtk::EventBox.new.add(Gtk::Image.new(@referencesGraphiques.getRefGraphique("gauche")));
@@ -44,11 +39,11 @@ class ZoneCtrl <  Gtk::Frame
     haut = Gtk::EventBox.new.add(Gtk::Image.new(@referencesGraphiques.getRefGraphique("haut")));
     bas = Gtk::EventBox.new.add(Gtk::Image.new(@referencesGraphiques.getRefGraphique("bas")));
     #association au controlleur
-    @controller.deplacementGaucheCreer(gauche);  
+    @controller.deplacementGaucheCreer(gauche);
     @controller.deplacementDroiteCreer(droite);
     @controller.deplacementHautCreer(haut);
     @controller.deplacementBasCreer(bas);
-    #creation bouton  
+    #creation bouton
     repos = Gtk::Button.new("REPOS");
     inventaire = Gtk::Button.new("INVENTAIRE");
     menu = Gtk::Button.new("MENU");
@@ -58,15 +53,12 @@ class ZoneCtrl <  Gtk::Frame
     @controller.inventaireCreer(inventaire);
     @controller.menuCreer(menu);
     @controller.interactionCreer(interaction);
-    
-    
-    
 
     vbox = Gtk::VBox.new(true, 3);
     hbox1 = Gtk::HBox.new(true, 3);
     hbox2 = Gtk::HBox.new(true, 3);
     hbox3 = Gtk::HBox.new(true, 3);
-    
+
     vbox.add(hbox1);
     vbox.add(hbox2);
     vbox.add(hbox3);
@@ -74,31 +66,21 @@ class ZoneCtrl <  Gtk::Frame
     hbox1.add(repos);
     hbox1.add(haut);
     hbox1.add(inventaire);
-    
+
     #ligne2
     hbox2.add(gauche);
     hbox2.add(Gtk::Label.new(""));
     hbox2.add(droite);
-    
+
     #ligne3
     hbox3.add(menu);
     hbox3.add(bas);
     hbox3.add(interaction);
-    
+
     add(vbox);
     show_all();
-    
+
   end
-  
-  
-  
-  
-  
-  
-  
-  
+
 end
-
-
-
 
