e0b86363a91414825e96a343a566c82e4e50f1a9
dragonxav <xavboxxavto@msn.com>
2013-05-06 10:14:01 +0200

    ajout de commentaires vue

--- Vue.rb
+++ Vue.rb
@@ -1,6 +1,12 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby 
 
-
+## 
+# Fichier            : Vue.rb 
+# Auteur            : L3SPI - Groupe de projet B 
+# Fait partie de : TheGame 
+# 
+# Cette classe représente l'affichage du jeu dan sa totalité 
+# 
 require 'gtk2'
 require './Zaf.rb'
 require './ZoneCtrl.rb'
@@ -8,12 +14,10 @@ require './Carte.rb'
 require './Bibliotheque/ReferencesGraphiques.rb'
 require './XMLReader/XmlRefGraphiquesReader.rb'
 
-
 class Vue
 
-  
   @vue; #affichage carte
-  @zaf; 
+  @zaf;
   @zoneCtrl;
   @carte; #la classe carte
   @referencesGraphiques; #fichier xml des images
@@ -24,9 +28,8 @@ class Vue
   @controller
   @x; #coordonné actuel
   @y; #coordonné actuel
-  
+
   attr_accessor :x ,:y;
-  
   def initialize()
     #Gtk.init();
     #@carteVue = Modele.carte;
@@ -34,98 +37,95 @@ class Vue
     #Gtk.main();
 
   end
-  
+
   def defM(modele)
     @modele = modele
   end
-  
+
   def defC(controller)
     @controller=controller
   end
-  
+
   def initInterface()
     Gtk.init();
-    
+
     @referencesGraphiques = ReferencesGraphiques.new()
     XmlRefGraphiquesReader.lireXml(@referencesGraphiques)
-    
+
     @hauteurAfficheCarte = 11
     @largeurAfficheCarte = 23
+    #matrice de stockage
     @vue = Array.new(@hauteurAfficheCarte){|x|Array.new(@largeurAfficheCarte ){|y|Gtk::Image.new()}}
     @zaf = Zaf.new()
     @zoneCtrl = ZoneCtrl.creer(@vue,@controller)
     @carte = @modele.carte
-    
-   
+
     window = Gtk::Window.new()
-       window.signal_connect('destroy') { 
-         Gtk.main_quit()
+    window.signal_connect('destroy') {
+      Gtk.main_quit()
     }
-    
+
     #tableau pour le bas de la fenêtre
     tabBot = Gtk::Table.new(1,3,true)
-    
+
     tabBot.attach(@zaf,0,2,0,1)
     tabBot.attach(@zoneCtrl,2,3,0,1)
-    
+
     #tableau pour afficher la carte
     @carteVue = Gtk::Table.new(@hauteurAfficheCarte,@largeurAfficheCarte,true)
-    
+
     vbox = Gtk::VBox.new()
-    
+
     #initialisation de la carte
-    
+
     initCarte(0,0)
     vbox.add(@carteVue)
     vbox.add(tabBot)
-     
+
     window.set_resizable(false)
     window.add(vbox)
-    
+
     window.set_title("THE GAME")
-    
+
     window.show_all()
- 
+
     Gtk.main();
   end
-  
+
   def initCarte(debutX,debutY)
-       0.upto(@hauteurAfficheCarte-1) do |x|
-       0.upto(@largeurAfficheCarte-1)do |y|
-           @carteVue.attach(@vue[x][y],y,y+1,x,x+1)
-       end
-     end
-     
-     afficheCarte(debutX,debutY)
-   end
-  
-  
+    0.upto(@hauteurAfficheCarte-1) do |x|
+      0.upto(@largeurAfficheCarte-1)do |y|
+        @carteVue.attach(@vue[x][y],y,y+1,x,x+1)
+      end
+    end
+
+    afficheCarte(debutX,debutY)
+  end
+
   def afficheCarte(debutX,debutY)
     @x=debutX
     @y=debutY
 
-
-     0.upto(@hauteurAfficheCarte-1) do |x|
+    0.upto(@hauteurAfficheCarte-1) do |x|
       0.upto(@largeurAfficheCarte-1)do |y|
         if(@carte.getCaseAt(x+debutX,y+debutY).joueur!=nil)
-          @vue[x][y].file=((@referencesGraphiques.getRefGraphique(@carte.getCaseAt(x+debutX,y+debutY).joueur().getIntitule().downcase)))           
+          @vue[x][y].file=((@referencesGraphiques.getRefGraphique(@carte.getCaseAt(x+debutX,y+debutY).joueur().getIntitule().downcase)))
         elsif(!@carte.getCaseAt(x+debutX,y+debutY).listeEnnemis.empty?())
-          @vue[x][y].file=((@referencesGraphiques.getRefGraphique("mechant")))          
+          @vue[x][y].file=((@referencesGraphiques.getRefGraphique("mechant")))
         else
-          @vue[x][y].file=((@referencesGraphiques.getRefGraphique(@carte.getCaseAt(x+debutX,y+debutY).getIntitule().downcase)))           
+          @vue[x][y].file=((@referencesGraphiques.getRefGraphique(@carte.getCaseAt(x+debutX,y+debutY).getIntitule().downcase)))
         end
-      end   
+      end
     end
   end
-
 
   def getZaf()
     return @zaf
   end
-  
+
   def actualiser
     afficheCarte(0,0)
   end
-  
+
 end
 
