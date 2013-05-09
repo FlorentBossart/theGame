b866f905c6ac8c987cc95073552c890a0ce50abe
FlorentBossart <florent.bossart@live.fr>
2013-05-09 09:18:27 +0200

    ptite maj/ debuggage

--- Vue.rb
+++ Vue.rb
@@ -68,7 +68,7 @@ class Vue
     @vue = Array.new(@hauteurAfficheCarte){|x|Array.new(@largeurAfficheCarte ){|y|Gtk::Image.new()}}
     @zaf = Zaf.new()
     @menu = MenuJeu.creer(true, @controller)
-    @zoneCtrl = ZoneCtrl.creer(@vue,@controller)
+    @zoneCtrl = ZoneCtrl.creer(self,@controller)
     @carte = @modele.carte
     @popUp=PopUp.creer(self)
     @interactionModal=InteractionModal.creer(@modele,self)
--- ZoneCtrl.rb
+++ ZoneCtrl.rb
@@ -13,6 +13,7 @@ require './XMLReader/XmlRefGraphiquesReader.rb'
 require './Controller.rb'
 
 class ZoneCtrl <  Gtk::Frame
+  private_class_method :new
   @referencesGraphiques
   @vue
   @controller
@@ -26,13 +27,13 @@ class ZoneCtrl <  Gtk::Frame
   @menu
   @interaction
   def initialize(vue,controller)
-    super(); #Frame
+    super() #Frame
     #Gtk.init();
     @vue=vue
     @controller=controller
-    @referencesGraphiques = ReferencesGraphiques.new();
-    XmlRefGraphiquesReader.lireXml(@referencesGraphiques);
-    initInterface();
+    @referencesGraphiques = ReferencesGraphiques.new()
+    XmlRefGraphiquesReader.lireXml(@referencesGraphiques)
+    initInterface()
 
     #Gtk.main();
   end
@@ -43,48 +44,48 @@ class ZoneCtrl <  Gtk::Frame
 
   def initInterface()
     #creation image bouton
-    @gauche = Gtk::EventBox.new.add(Gtk::Image.new(@referencesGraphiques.getRefGraphique("gauche")));
-    @droite = Gtk::EventBox.new.add(Gtk::Image.new(@referencesGraphiques.getRefGraphique("droite")));
-    @haut = Gtk::EventBox.new.add(Gtk::Image.new(@referencesGraphiques.getRefGraphique("haut")));
-    @bas = Gtk::EventBox.new.add(Gtk::Image.new(@referencesGraphiques.getRefGraphique("bas")));
+    @gauche = Gtk::EventBox.new.add(Gtk::Image.new(@referencesGraphiques.getRefGraphique("gauche")))
+    @droite = Gtk::EventBox.new.add(Gtk::Image.new(@referencesGraphiques.getRefGraphique("droite")))
+    @haut = Gtk::EventBox.new.add(Gtk::Image.new(@referencesGraphiques.getRefGraphique("haut")))
+    @bas = Gtk::EventBox.new.add(Gtk::Image.new(@referencesGraphiques.getRefGraphique("bas")))
     #association au controlleur
-    @controller.deplacementGaucheCreer(@gauche);
-    @controller.deplacementDroiteCreer(@droite);
-    @controller.deplacementHautCreer(@haut);
-    @controller.deplacementBasCreer(@bas);
+    @controller.deplacementGaucheCreer(@gauche)
+    @controller.deplacementDroiteCreer(@droite)
+    @controller.deplacementHautCreer(@haut)
+    @controller.deplacementBasCreer(@bas)
     #creation bouton
-    @repos = Gtk::Button.new("REPOS");
-    @inventaire = Gtk::Button.new("INVENTAIRE");
-    @menu = Gtk::Button.new("MENU");
-    @interaction = Gtk::Button.new("INTERACTON");
+    @repos = Gtk::Button.new("REPOS")
+    @inventaire = Gtk::Button.new("INVENTAIRE")
+    @menu = Gtk::Button.new("MENU")
+    @interaction = Gtk::Button.new("INTERACTON")
     #association au controler
-    @controller.reposCreer(@repos);
-    @controller.inventaireCreer(@inventaire);
-    @controller.menuCreer(@menu);
-    @controller.interactionCreer(@interaction);
+    @controller.reposCreer(@repos)
+    @controller.inventaireCreer(@inventaire)
+    @controller.menuCreer(@menu)
+    @controller.interactionCreer(@interaction)
 
-    vbox = Gtk::VBox.new(true, 3);
-    hbox1 = Gtk::HBox.new(true, 3);
-    hbox2 = Gtk::HBox.new(true, 3);
-    hbox3 = Gtk::HBox.new(true, 3);
+    vbox = Gtk::VBox.new(true, 3)
+    hbox1 = Gtk::HBox.new(true, 3)
+    hbox2 = Gtk::HBox.new(true, 3)
+    hbox3 = Gtk::HBox.new(true, 3)
 
-    vbox.add(hbox1);
-    vbox.add(hbox2);
-    vbox.add(hbox3);
+    vbox.add(hbox1)
+    vbox.add(hbox2)
+    vbox.add(hbox3)
     #ligne 1
-    hbox1.add(@repos);
-    hbox1.add(@haut);
-    hbox1.add(@inventaire);
+    hbox1.add(@repos)
+    hbox1.add(@haut)
+    hbox1.add(@inventaire)
 
     #ligne2
-    hbox2.add(@gauche);
-    hbox2.add(@Gtk::Label.new(""));
-    hbox2.add(@droite);
-
+    hbox2.add(@gauche)
+    hbox2.add(Gtk::Label.new(""))
+    hbox2.add(@droite)
+    
     #ligne3
-    hbox3.add(@menu);
-    hbox3.add(@bas);
-    hbox3.add(@interaction);
+    hbox3.add(@menu)
+    hbox3.add(@bas)
+    hbox3.add(@interaction)
 
     add(vbox);
     show_all();
