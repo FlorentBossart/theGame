f3bb495f4a619828bc02e84737bef63a3795cf7a
ludovic704 <ludovic.roucoux@gmail.com>
2013-05-12 19:24:03 +0200

    Save de la classe modele dans un fichier slot.yaml

--- YamlSlot.rb
+++ YamlSlot.rb
@@ -79,6 +79,10 @@ class YamlSlot
 	      puts "temps jeu : " + modele.joueur.tempsTotal.to_s
 	      file.syswrite(modele.joueur.tempsTotal.to_yaml())
 	      
+	      #puts modele.joueur
+	     # file.syswrite(modele.joueur.to_yaml())
+	      puts modele
+	      file.syswrite(modele.to_yaml())
 	      #puts "carte : "
 	      #p modele.carte
 	      #file.syswrite(modele.carte.to_yaml())
